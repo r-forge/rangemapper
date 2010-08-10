45b287d83f3446ef925edea12e64e2a431509acf
valcu (remove_account remove_account) <valcu@orn.mpg.de>
2010-08-10 13:32:46 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rangemapper@8 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/index.php
+++ www/index.php
@@ -43,9 +43,37 @@ echo $contents; } ?>
 
 <!-- end of project description -->
 
-<p> No content added. </p>
+rangeMapper is a suite of tools for easy generation of biodiversity (species richness) or life-history traits maps and, in general, maps of any variable associated with a species or population. rangeMapper performs range maps interpolation
+with a pre-defined grid, then computes at each grid cell a chosen statistical model. rangeMapper can be easily extended with any statistical model (from a simple average to mixed-effect and phylogenetic models) available in one of the existing R packages. The resulting raster maps are stored in a rangeMapper project file (a pre-customized SQLite database) and can thus be displayed
+and/or manipulated at any latter stage. rangeMapper is built on the framework provided by the sp, maptools and rgdal packages using sqlite support to store data. rangeMapper comes with a user-friendly platform-independent tcltk graphical user interface.
+
+
+<p> <strong>Package vignette(s) :</strong> 
+<ul>
+<li> <a href="rangeMapper.pdf">Graphical user interface</a> 
+</ul>
+
+
+
 
 <p> The <strong>project summary page</strong> you can find <a href="http://<?php echo $domain; ?>/projects/<?php echo $group_name; ?>/"><strong>here</strong></a>. </p>
 
 </body>
 </html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
