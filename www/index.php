68a7fc526d1de10fa09666347e91a7c18344ee32
valcu (remove_account remove_account) <valcu@orn.mpg.de>
2010-09-07 06:52:31 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rangemapper@18 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/index.php
+++ www/index.php
@@ -21,34 +21,52 @@ $group_name=ereg_replace('([^\.]*)\..*$','\1',$_SERVER['HTTP_HOST']);
 
 <body>
 
-<h1>  rangeMapper project  </h1>
+<h1>  rangeMapper - A package for easy generation of biodiversity (species richness) or life-history traits maps using species geographical  ranges </h1> 	  
 
-<ul class="list-tick">
-<li> is a suite of tools for easy generation of biodiversity (species richness) or life-history traits maps and, in general, maps of any variable associated with a species or population. </li>
-<li> performs range maps interpolation with a pre-defined grid, then computes at each grid cell a chosen statistical model. </li>
-<li> can be extended with any statistical model (from a simple average to mixed-effect and phylogenetic models) available in one of the existing R packages.</li>
+<h2>  rangeMapper project  </h2> 
+
+<ul class="tick">
+<li> A suite of tools for easy generation of biodiversity (species richness) or life-history traits maps and, in general, maps of any variable associated with a species or population. </li>
+<li> Performs range maps interpolation with a pre-defined grid, then computes at each grid cell a chosen statistical model. </li>
+<li> Can be extended with any statistical model (from a simple average to mixed-effect and phylogenetic models) available in one of the existing R packages.</li>
 <li> The resulting raster maps are stored in a rangeMapper project file (a pre-customized SQLite database) and can thus be displayed and/or manipulated at any latter stage.  </li>
-<li> is built on the framework provided by the sp, maptools and rgdal packages using sqlite support to store data. </li>
-<li> has a user-friendly platform-independent graphical user interface.  </li>
+<li> Is built on the framework provided by the sp, maptools and rgdal packages using sqlite support to store data. </li>
+<li> Has a user-friendly platform-independent graphical user interface.  </li>
 </ul>
  
 
+ 
 <h2> Package vignette(s) :</h2>
-<ul> 
-<li><a href="rangeMapper.pdf">Graphical user interface</a>  </li> 
+<ul class="bull"> 
+<li><a href="rangeMapper.pdf">Graphical user interface, quick guide.</a>  </li> 
 </ul>
 
+<h2> Download and install:</h2>
+<ul class="bull"> 
+<li><a href="https://r-forge.r-project.org/R/?group_id=856">Download page</a>  </li> 
+<li>To install this package directly within R type: install.packages("rangeMapper", repos="http://R-Forge.R-project.org")  </li> 
+</ul>
+ 
 
 <h2> The main GUI</h2>
 <img src="gui.png"  class="center" />
 
-<h2> A map created with rangeMapper: mammalian species richness</h2>
-<img src="map1d.png"  class="center" />
 
-<h3> The geographical range data of mammals of the world (5227 species) is available
-online at <a href="http://www.iucnredlist.org/technical-documents/spatial-data"> IUCN </a>. The rangeMapper project was created using a 1.5deg
-canvas. 
-</h3>
+<h2> Examples:</h2>
+<ul class="bull"> 
+<li> Mammalian species richness </li> 
+	<h3> The geographical range data of mammals of the world (5227 species) is available online at <a href="http://www.iucnredlist.org/technical-documents/spatial-data"> IUCN </a>. The rangeMapper project was created using a 1.5deg canvas. 
+	</h3>
+<img src="map1d.png"  class="center" />
+</ul>
+ 
+
+
+
+
+
+
+
 
 
 <hr class="a">
