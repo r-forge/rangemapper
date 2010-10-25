4ceb376c72059c10fc247cb8399bf015bba69dbb
valcu (remove_account remove_account) <valcu@orn.mpg.de>
2010-10-25 21:36:37 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rangemapper@54 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/utils.R
+++ pkg/R/utils.R
@@ -1,20 +1,4 @@
 
-# force a function, taking a vector argument, to take a data.frame argument using formula, data.
-force.formula <- function(f) {
-	funtxt = paste(c("function(formula, data, fun){" ,
-				"v = as.character(formula[[2L]])", 
-				"x = na.omit(data[, v])", 
-				'f<-', 
-				paste(deparse(f), collapse = '\n'),
-				"f(x) }"), collapse = "\n")
-				
-	return(eval(parse(text = funtxt)))
-}
-
-# Example
-# F =  force.formula(function(x) {mean(x)/sd(x)} ) 
-# F(x ~ 1, data = data.frame(x = rnorm(100)), function(x) mean(x)/sd(x) )
-
 Msg <- function(msg, tkMainWindow = "win", tkElement = "msg", eol = "\n", keep = FALSE, clearup = FALSE, getTime = FALSE, envir = ".RangeMapper") {
 
   if(getTime) msg = paste( "<", Sys.time(), ">\n", msg, sep = "")
@@ -56,6 +40,23 @@ Msg <- function(msg, tkMainWindow = "win", tkElement = "msg", eol = "\n", keep =
     invisible(flush.console() )
 	
 } 
+
+force.formula <- function(f) {
+# force a function, taking a vector argument, to take a data.frame argument using formula, data.	
+	funtxt = paste(c("function(formula, data, fun){" ,
+				"v = as.character(formula[[2L]])", 
+				"x = na.omit(data[, v])", 
+				'f<-', 
+				paste(deparse(f), collapse = '\n'),
+				"f(x) }"), collapse = "\n")
+				
+	return(eval(parse(text = funtxt)))
+
+# Example
+# F =  force.formula(function(x) {mean(x)/sd(x)} ) 
+# F(x ~ 1, data = data.frame(x = rnorm(100)), function(x) mean(x)/sd(x) )	
+	
+}
 
 brewer.pal.get <- function(palette = NULL) {
 	pal = brewer.pal.info[, ]
--- pkg/R/zzz.R
+++ pkg/R/zzz.R
@@ -1,19 +1,17 @@
 
-.First.lib     <- function(lib, pkg) {
+.onLoad <- function(lib, pkg) {
+	require(methods)
 	require(utils)
-	v = packageDescription("rangeMapper")$Version
 	sep = paste("\n",paste(rep("-", 61),collapse = ""),"\n", collapse = "")
-	cat(paste(sep, "This is rangeMapper", v,
+	cat(paste(sep, "This is", pkg, packageDescription("rangeMapper")$Version,
 				   "\n   Type", sQuote("rangeMapper()"), "to start the graphical user interface.",
 				   "\n   Type", sQuote("?rangeMapper"), "to access the help files.",
 				   
-			 sep))
+			 sep, fill=TRUE))
 	
 	
 	
 	}
-
-
 
 
 
